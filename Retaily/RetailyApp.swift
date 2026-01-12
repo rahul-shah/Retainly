c87bc10357428d330ee5391ed5a6061ecebe01d5
Rahul Shah <shahdotrahul@gmail.com>
2026-01-12 11:59:01 +0000

    Initial Commit

---
+++ Retaily/RetailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailyApp.swift
+//  Retaily
+//
+//  Created by Rahul Shah on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct RetailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RetailyTests/RetailyTests.swift
@@ -0,0 +1,16 @@
+//
+//  RetailyTests.swift
+//  RetailyTests
+//
+//  Created by Rahul Shah on 12/01/2026.
+//
+
+import Testing
+
+struct RetailyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
